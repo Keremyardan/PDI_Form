5b8a735c0ad47934d92c41411a69cbe7f3dacb49
Keremyardan <keremyrdn@hotmail.com>
2025-04-28 15:49:55 +0300

    loginscreen has been created

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,13 +1,24 @@
-import '../src/App.css'
-import PdiForm from './components/PdiForm'
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import PdiForm from './components/PdiForm';
+import Login from './components/Login';
 
 function App() {
+  const [role, setRole] = useState(null);
+
+  const handleLogin = (role) => {
+    setRole(role);
+  };
 
   return (
-    <>
-    <PdiForm/>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login onLogin={handleLogin} />} />
+        <Route path="/pdi-form" element={role ? <PdiForm /> : <Navigate to="/login" />} />
+        <Route path="*" element={<Navigate to="/login" />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
