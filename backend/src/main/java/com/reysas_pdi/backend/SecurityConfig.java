b81ad65c2d5c32b3ef0603b9a31350c900a9ac52
Keremyardan <keremyrdn@hotmail.com>
2025-07-01 10:44:26 +0300

    variable names corrected

--- backend/src/main/java/com/reysas_pdi/backend/SecurityConfig.java
+++ backend/src/main/java/com/reysas_pdi/backend/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain fiterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .cors(Customizer.withDefaults())
                 .csrf(csrf -> csrf.disable())
